b544de9d1308b75090606ce8b3c6cbffae8c0f31
PITIFULHAWK <xxxxyyyy1920@gmail.com>
2024-09-18 17:21:16 +0530

    Added Content to all pages

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -6,7 +6,7 @@ export default function ArticlePage() {
                 My Articles
             </h1>
             <p className='text-sm lg:text-base font-normal max-w-xl mt-4'>
-                Ever since I was a kid, I've been fascinated by technology.
+                Ever since I was a kid, I&apos;ve been fascinated by technology.
             </p>
             <h2 className='lg:text-2xl md:text-lg text-[#AEAEAE] text-base font-semibold mb-4 mt-20'>
                 No Articles writen.
