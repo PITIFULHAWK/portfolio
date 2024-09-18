5734c352dbafa8e2b4f3d01f25e4e1daeeedf52d
PITIFULHAWK <xxxxyyyy1920@gmail.com>
2024-09-18 14:17:43 +0530

    added footer and made site responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
                     <SideBarWrapper />
 
                     <div className='lg:pl-2 lg:pb-1 lg:pt-2 flex-1 overflow-y-auto'>
-                        <div className='flex-1 bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 min-h-screen lg:rounded-xl border border-transparent lg:border-gray-700 overflow-y-auto flex flex-col mr-2'>
+                        <div className='flex-1 bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 min-h-screen lg:rounded-xl border border-transparent lg:border-gray-700 overflow-y-auto flex flex-col mr-0 lg:mr-2'>
                             <main className='flex-grow max-w-4xl w-full mx-auto py-20 px-4 md:px-10 text-white'>
                                 {children}
                             </main>
